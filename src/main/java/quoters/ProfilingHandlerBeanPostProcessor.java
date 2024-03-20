fe424516c4aa24d3b29763d2a1d653a6fec12e77
vit73123 <vit73123@gmail.com>
2024-03-20 20:10:15 +0300

    P1: ManagementFactory, MBeanServer: Register ProfilingController

--- src/main/java/quoters/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/quoters/ProfilingHandlerBeanPostProcessor.java
@@ -3,6 +3,10 @@ package quoters;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -12,6 +16,11 @@ import java.util.Map;
 public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
     private Map<String, Class> map = new HashMap<>();
     private ProfilingController controller = new ProfilingController();
+
+    public ProfilingHandlerBeanPostProcessor() throws Exception {
+        MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
+        platformMBeanServer.registerMBean(controller, new ObjectName("profiling", "name", "controller"));
+    }
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
