69aa00f38fd8cc10152638fbd88c542325bff886
vit73123 <vit73123@gmail.com>
2024-10-12 13:41:24 +0300

    P2: BeanFactoryPostProcessor

--- src/main/java/quoters/T100.java
+++ src/main/java/quoters/T100.java
@@ -1,6 +1,6 @@
 package quoters;
 
-public class T100 extends TerminatorQuoter {
+public class T100 extends TerminatorQuoter implements Quoter {
     public void sayQuote() {
         System.out.println("Я жидкий");
     }
