71284533984c6dc53385577d4b062e04c2f10e18
vit73123 <vit73123@gmail.com>
2024-10-12 15:38:43 +0300

    P2: @Scope: refresh prototype in singleton: Correct

--- src/main/java/screensaver/ColorFrame.java
+++ src/main/java/screensaver/ColorFrame.java
@@ -1,6 +1,5 @@
 package screensaver;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.swing.*;
@@ -8,10 +7,7 @@ import java.awt.*;
 import java.util.Random;
 
 @Service
-public class ColorFrame extends JFrame {
-    @Autowired
-    private Color color;
-
+public abstract class ColorFrame extends JFrame {
     public ColorFrame() {
         setSize(200, 200);
         setVisible(true);
@@ -21,7 +17,9 @@ public class ColorFrame extends JFrame {
     public void showOnRandomPlace() {
         Random random = new Random();
         setLocation(random.nextInt(1200), random.nextInt(700));
-        getContentPane().setBackground(color);
+        getContentPane().setBackground(getColor());
         repaint();
     }
+
+    protected abstract Color getColor();
 }
